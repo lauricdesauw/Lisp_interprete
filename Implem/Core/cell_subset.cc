cdfe1f3e55cd3527dd13a0861ae8012f916d63d3
Clara <clara.begue@ens-rennes.fr>
2019-02-27 10:44:00 +0100

    cell_subset made

--- Implem/Core/cell_subset.cc
+++ Implem/Core/cell_subset.cc
@@ -1,4 +1,80 @@
 #include <string>
 #include "cell_subset.hh"
 
-/****************/
+/******* Cell_number *******/
+
+Cell_number::Cell_number (int n)
+{
+    Cell::Cell();
+    contents = n;
+}
+
+type Cell_number::get_type ()
+{
+    return Cell::NUMBER;
+}
+
+int Cell_number::get_contents ()
+{
+    return contents;
+}
+
+/******* Cell_string *******/
+
+Cell_string::Cell_string (std::string s)
+{
+    Cell::Cell();
+    contents = s;
+}
+
+type Cell_string::get_type ()
+{
+    return Cell::STRING;
+}
+
+std::string Cell_string::get_contents ()
+{
+    return contents;
+}
+
+/******* Cell_symbol *******/
+
+Cell_symbol::Cell_symbol (std::string s)
+{
+    Cell::Cell();
+    contents = s;
+}
+
+type Cell_symbol::get_type ()
+{
+    return Cell::SYMBOL;
+}
+
+std::string Cell_symbol::get_contents ()
+{
+    return contents;
+}
+
+/******* Cell_pair *******/
+
+Cell_pair::Cell_pair (Cell* _car, Cell* _cdr)
+{
+    Cell::Cell();
+    car = _car;
+    cdr = _cdr;
+}
+
+type Cell_pair::get_type ()
+{
+    return Cell::PAIR;
+}
+
+Cell* Cell_pair::get_car ()
+{
+    return car;
+}
+
+Cell* Cell_pair::get_cdr ()
+{
+    return cdr;
+}
--- Interface/Core/cell_subset.hh
+++ Interface/Core/cell_subset.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include "cell.hh"
 
 class API;
 
@@ -9,7 +10,7 @@ class Cell_number : public Cell {
     private:
         int contents;
 
-    private:
+    public:
         Cell_number(int n);
         type get_type() const override;
         int get_contents() const;
@@ -23,7 +24,7 @@ class Cell_string : public Cell {
     private:
         std::string contents;
 
-    private:
+    public:
         Cell_string(std::string s);
         type get_type() const override;
         std::string get_contents() const;
@@ -37,7 +38,7 @@ class Cell_symbol : public Cell {
     private:
         std::string contents;
 
-    private:
+    public:
         Cell_symbol(std::string s);
         type get_type() const override;
         std::string get_contents() const;
@@ -52,7 +53,7 @@ class Cell_pair : public Cell {
         Cell *car;
         Cell *cdr;
 
-    private:
+    public:
         Cell_pair(Cell *_car, Cell *_cdr);
         type get_type() const override;
         Cell *get_car() const;
