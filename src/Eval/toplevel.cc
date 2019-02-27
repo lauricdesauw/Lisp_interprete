a0ddd58c710cbd434f3982ecfdaab05d9d7d6f30
Clara <clara.begue@ens-rennes.fr>
2019-02-27 16:32:33 +0100

    toplevel test

--- src/Eval/toplevel.cc
+++ src/Eval/toplevel.cc
@@ -1,10 +1,36 @@
 #include "toplevel.hh"
 #include "read.hh"
 #include "library.hh"
+#include <iostream>
 
 Toplevel::Toplevel()
 {
     global_env = *(new Env());
+}
+
+std::ostream& print_type(std::ostream& s, Object l)
+{
+    if (numberp(l))
+    {
+        s << "It's a number." << std::endl;
+    }
+    else if (symbolp(l))
+    {
+        s << "It's a symbol." << std::endl;
+    }
+    else if (stringp(l))
+    {
+        s << "It's a string." << std::endl;
+    }
+    else if (boolp(l))
+    {
+        s << "It's a bool." << std::endl;
+    }
+    else if (listp(l))
+    {
+        s << "It's a list." << std::endl;
+    }
+    return s;
 }
 
 void Toplevel::go(bool use_prompt)
@@ -12,8 +38,10 @@ void Toplevel::go(bool use_prompt)
 
     while(true)
     {
-        std::cout << "C++Lisp :";
+        std::cout << "C++Lisp: ";
         Object curr_obj = read_object();
-        print_object(std::cout, curr_obj);
+        print_type(std::cout,curr_obj);
+        std::cout << curr_obj;
+        std::cout << std::endl;om
     }
 }
