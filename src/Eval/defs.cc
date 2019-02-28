6d345c80c6275d60fe512f9cda6aefbbb23c99ad
PaulBastide <paul.bastide@ens-rennes.fr>
2019-02-28 09:04:15 +0100

    corecction subr

--- src/Eval/defs.cc
+++ src/Eval/defs.cc
@@ -4,20 +4,20 @@ Object do_if(Object l, Env env){}
 
 Object do_plus(Object lvals)
 {
-    return( object_to_number(car(lvals)) + object_to_number(cadr(lvals)) );
+    return number_to_object( object_to_number(car(lvals)) + object_to_number(cadr(lvals)) );
 }
 
 Object do_minus(Object lvals)
 {
-    return( object_to_number(car(lvals)) - object_to_number(cadr(lvals)) );
+    return number_to_object( object_to_number(car(lvals)) - object_to_number(cadr(lvals)) );
 }
 
 Object do_times(Object lvals)
 {
-    return( object_to_number(car(lvals)) * object_to_number(cadr(lvals)) );
+    return number_to_object( object_to_number(car(lvals)) * object_to_number(cadr(lvals)) );
 }
 
 Object do_eq(Object lvals)
 {
-    return( eq(car(lvals),cadr(lvals)) );
+    return number_to_object( eq(car(lvals),cadr(lvals)) );
 }
