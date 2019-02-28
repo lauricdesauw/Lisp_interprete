9145def0fd7eedc0207d8c57ddffddef92108707
Clara <clara.begue@ens-rennes.fr>
2019-02-28 11:22:40 +0100

    eval

--- src/Eval/defs.cc
+++ src/Eval/defs.cc
@@ -4,17 +4,20 @@ Object do_if(Object l, Env env){}
 
 Object do_plus(Object lvals)
 {
-    return number_to_object( object_to_number(car(lvals)) + object_to_number(cadr(lvals)) );
+    return number_to_object( object_to_number(car(lvals))
+                            + object_to_number(cadr(lvals)) );
 }
 
 Object do_minus(Object lvals)
 {
-    return number_to_object( object_to_number(car(lvals)) - object_to_number(cadr(lvals)) );
+    return number_to_object( object_to_number(car(lvals))
+                            - object_to_number(cadr(lvals)) );
 }
 
 Object do_times(Object lvals)
 {
-    return number_to_object( object_to_number(car(lvals)) * object_to_number(cadr(lvals)) );
+    return number_to_object( object_to_number(car(lvals))
+                            * object_to_number(cadr(lvals)) );
 }
 
 Object do_eq(Object lvals)
--- src/Eval/eval.cc
+++ src/Eval/eval.cc
@@ -1,35 +1,29 @@
 #include <string>
 #include <iostream>
+#include <cassert>
 #include "eval.hh"
 #include "library.hh"
+#include "defs.hh"
 
 Object get_value_env(Object l, Env env)
 {
-    
+
 }
 
 Object eval (Object l, Env env)
 {
-    if (null(l) || stringp(l) || numberp(l))
+    if (null(l) || stringp(l) || numberp(l) || boolp(l))
     {
         return l;
     }
     else if (symbolp(l))
     {
-        std::string s = object_to_string(l);
-        if (s == "#t")
-        {
-            return t();
-        }
-        else if (s == "#f")
-        {
-            return f();
-        }
-        else if (s == "nil")
-        {
-            return nil();
-        }
         return get_value_env(l,env);
     }
-    else {return nil();}
+    else
+    {
+        assert(listp(l));
+        Object f = car(l);
+        return (nil());
+    }
 }
