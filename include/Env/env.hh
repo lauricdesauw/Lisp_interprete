cc3dda88ff2135d41924aa047b32129f220e3304
PaulBastide <paul.bastide@ens-rennes.fr>
2019-03-01 16:30:40 +0100

    Tu es le fils d'un dieu mais l'enfant du péché

--- include/Env/env.hh
+++ include/Env/env.hh
@@ -3,12 +3,12 @@
 #include "library.hh"
 #include "error.hh"
 
+bool bindingp(Object obj);
+Object binding_value(Object obj);
+std::string binding_name(Object obj);
+
 Env make_env();
 Env add_new_binding(std::string name, Object value, Env env);
 Env extend_largs_env(Object lpars, Object lvals, Env env);
 Object find_value(Object obj, Env env);
 std::ostream& print_env(std::ostream& s, Env env);
-
-bool bidingp(Object obj);
-Object biding_value(Object obj);
-std::string biding_name(Object obj);
